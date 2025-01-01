c4d1ec6757c3e5297ef42d0f038a636ec5922a84
coderomm <mail.coderom@gmail.com>
2025-01-01 23:43:48 +0530

    Init ws and http server

---
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+export const router = Router();
+
+router.get('/signup', (req, res) => {
+    res.json({
+        message: "signup"
+    })
+})
+
+router.get('/signin', (req, res) => {
+    res.json({
+        message: "signin"
+    })
+})
