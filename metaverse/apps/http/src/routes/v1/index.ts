a4a39d55b42ad2e1cf58c00f02d6ae7fa5d55890
coderomm <mail.coderom@gmail.com>
2025-01-02 23:56:53 +0530

    Write user auth endpoints

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,15 +1,80 @@
 import { Router } from "express";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
+import { compare, hash } from "../../scrypt";
+import client from "@repo/db/client";
+import 'dotenv/config'
+import { SignupSchema, SigninSchema } from '../../schema-types';
+import jwt from "jsonwebtoken";
 
 export const router = Router();
 
-router.get('/signup', (req, res) => {
-    res.json({
-        message: "signup"
-    })
+router.post('/signup', async (req, res) => {
+    const parsedData = SignupSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.status(400).json({ message: "Validation failed" });
+        return;
+    }
+    const hashedPassword = await hash(parsedData.data.password);
+
+    try {
+        const user = await client.user.create({
+            data: {
+                username: parsedData.data.username,
+                password: hashedPassword,
+                role: parsedData.data.type === "admin" ? "Admin" : "User"
+            }
+        })
+        res.json({ userId: user.id })
+    } catch (error) {
+        res.status(400).json({ message: "User already exists" })
+    }
 })
 
-router.get('/signin', (req, res) => {
-    res.json({
-        message: "signin"
-    })
+router.post('/signin', async (req, res) => {
+    const parsedData = SigninSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.status(403).json({ message: "Validation failed" })
+        return
+    }
+    try {
+        const user = await client.user.findUnique({
+            where: {
+                username: parsedData.data.username
+            }
+        })
+
+        if (!user) {
+            res.status(403).json({ message: "User not found" })
+            return
+        }
+
+        const isValid = await compare(parsedData.data.password, user.password)
+
+        if (!isValid) {
+            res.status(403).json({ message: "Invalid password" })
+            return
+        }
+
+        const token = jwt.sign({
+            userId: user.id,
+            role: user.role
+        }, process.env.JWT_SECRATE || 'JWT_SECRATE');
+
+        res.json({ token })
+    } catch (e) {
+        res.status(400).json({ message: "Internal server error" })
+    }
 })
+
+router.get('/elements', (req, res) => {
+    res.json({ message: "elements" });
+})
+router.get('/avatars', (req, res) => {
+    res.json({ message: "avatars" })
+})
+
+router.use('/user', userRouter);
+router.use('/space', spaceRouter);
+router.use('/admin', adminRouter);
---
+++ metaverse/apps/http/src/routes/v1/user.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+export const userRouter = Router();
+
+userRouter.use('/metadata', (req, res) => {
+
+})
+
+userRouter.use('/metadata/bulk', (req, res) => {
+
+})
