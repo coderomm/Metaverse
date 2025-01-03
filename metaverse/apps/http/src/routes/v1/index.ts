9dca080c228d775e072aa1c1de88a47c88417fb6
coderomm <mail.coderom@gmail.com>
2025-01-03 08:36:22 +0530

    Passed user auth tests

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -11,13 +11,16 @@ import jwt from "jsonwebtoken";
 export const router = Router();
 
 router.post('/signup', async (req, res) => {
+    console.log('> inside signup')
+    console.log('> signup req.body = ', req.body)
     const parsedData = SignupSchema.safeParse(req.body);
+    console.log('> signup parsedData = ', parsedData)
     if (!parsedData.success) {
         res.status(400).json({ message: "Validation failed" });
         return;
     }
     const hashedPassword = await hash(parsedData.data.password);
-
+    console.log('> signup hashedPassword = ', hashedPassword)
     try {
         const user = await client.user.create({
             data: {
@@ -26,14 +29,19 @@ router.post('/signup', async (req, res) => {
                 role: parsedData.data.type === "admin" ? "Admin" : "User"
             }
         })
+        console.log('> signup user = ', user)
         res.json({ userId: user.id })
     } catch (error) {
+        console.log('> signup error = ', error)
         res.status(400).json({ message: "User already exists" })
     }
 })
 
 router.post('/signin', async (req, res) => {
+    console.log('> inside signin')
+    console.log('> signin req.body = ', req.body)
     const parsedData = SigninSchema.safeParse(req.body);
+    console.log('> signin parsedData = ', parsedData)
     if (!parsedData.success) {
         res.status(403).json({ message: "Validation failed" })
         return
@@ -44,7 +52,7 @@ router.post('/signin', async (req, res) => {
                 username: parsedData.data.username
             }
         })
-
+        console.log('> signin user = ', user)
         if (!user) {
             res.status(403).json({ message: "User not found" })
             return
@@ -61,9 +69,10 @@ router.post('/signin', async (req, res) => {
             userId: user.id,
             role: user.role
         }, process.env.JWT_SECRATE || 'JWT_SECRATE');
-
+        console.log('> signin token = ', token)
         res.json({ token })
     } catch (e) {
+        console.log('> signin error = ', e)
         res.status(400).json({ message: "Internal server error" })
     }
 })
