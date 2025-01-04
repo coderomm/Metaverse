a8b72b4710d4de96607a6aaa516085c1ada4fa76
coderomm <mail.coderom@gmail.com>
2025-01-04 23:07:00 +0530

    Passed all tests except ws

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -77,11 +77,29 @@ router.post('/signin', async (req, res) => {
     }
 })
 
-router.get('/elements', (req, res) => {
-    res.json({ message: "elements" });
+router.get("/elements", async (req, res) => {
+    const elements = await client.element.findMany()
+
+    res.json({
+        elements: elements.map(e => ({
+            id: e.id,
+            imageUrl: e.imageUrl,
+            width: e.width,
+            height: e.height,
+            static: e.static
+        }))
+    })
 })
-router.get('/avatars', (req, res) => {
-    res.json({ message: "avatars" })
+
+router.get("/avatars", async (req, res) => {
+    const avatars = await client.avatar.findMany()
+    res.json({
+        avatars: avatars.map(x => ({
+            id: x.id,
+            imageUrl: x.imageUrl,
+            name: x.name
+        }))
+    })
 })
 
 router.use('/user', userRouter);
