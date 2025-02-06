48878bdb1aa056866471b20aae72d5ab8ba79571
coderomm <mail.coderom@gmail.com>
2025-02-07 00:04:32 +0530

    Add sort by new in avatar list with new migration

--- towny/apps/frontend/src/pages/protected/ProfilePage.tsx
+++ towny/apps/frontend/src/pages/protected/ProfilePage.tsx
@@ -1,4 +1,3 @@
-// src/pages/ProfilePage.tsx
 import { useState, useEffect } from 'react';
 import { api } from '../../services/api';
 import { useAuth } from '../../context/AuthContext';
@@ -6,15 +5,17 @@ import { useNavigate } from 'react-router-dom';
 import Section from '../../components/ui/Section';
 import { Button } from '../../components/ui/Button';
 import { TextInput } from '../../components/ui/TextInput';
-import { CircleCheck } from 'lucide-react';
+import { ArrowDownUp, CircleCheck } from 'lucide-react';
 import { toast } from 'sonner';
 import SpinLoader from '../../components/ui/SpinLoader';
 import PageWrapper from '../../components/ui/PageWrapper';
 
 interface Avatar {
     id: number;
-    imageUrl: string;
-    name: string;
+    name: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+    imageUrl: string | null;
 }
 
 export const ProfilePage = () => {
@@ -22,6 +23,7 @@ export const ProfilePage = () => {
     const [selectedAvatarId, setSelectedAvatarId] = useState<number | null>(null);
     const [searchQuery, setSearchQuery] = useState('');
     const [isSaving, setIsSaving] = useState(false);
+    const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('desc');
     const navigate = useNavigate();
     const { isAuthenticated, fetchCurrentUser, isLoading } = useAuth();
 
@@ -38,14 +40,29 @@ export const ProfilePage = () => {
     const fetchAvatars = async () => {
         try {
             const response = await api.get('/avatars');
-            setAvatars(response.data.avatars);
+            let avatarList = response.data.avatars;
+            avatarList = avatarList.sort((a: Avatar, b: Avatar) => {
+                if (sortOrder === 'desc') {
+                    return (b.createdAt ? new Date(b.createdAt).getTime() : b.id) -
+                        (a.createdAt ? new Date(a.createdAt).getTime() : a.id);
+                } else {
+                    return (a.createdAt ? new Date(a.createdAt).getTime() : a.id) -
+                        (b.createdAt ? new Date(b.createdAt).getTime() : b.id);
+                }
+            });
+            setAvatars(avatarList);
         } catch (error) {
             console.error('Failed to fetch avatars:', error);
         }
     };
 
+    const handleSortToggle = () => {
+        setSortOrder(prev => (prev === 'asc' ? 'desc' : 'asc'));
+        fetchAvatars();
+    };
+
     const filteredAvatars = avatars.filter(avatar =>
-        avatar.name.toLowerCase().includes(searchQuery.toLowerCase())
+        avatar.name?.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
     const handleSave = async () => {
@@ -74,8 +91,8 @@ export const ProfilePage = () => {
 
     return (
         <PageWrapper>
-            <Section>
-                <div className="flex justify-between items-center mb-10">
+            <Section className='pt-0'>
+                <div className="flex justify-between items-center mb-4">
                     <h1 className="text-2xl font-bold">Profile Settings</h1>
                     <Button
                         onClick={handleSave}
@@ -97,6 +114,17 @@ export const ProfilePage = () => {
                         label='Select Avatar'
                     />
 
+                    <div className="flex items-start justify-between">
+                        <p className='text-gray-700 font-medium text-[14px] w-2/3'>Select an avatar to represent you in the Towny community.</p>
+                        <button
+                            onClick={handleSortToggle}
+                            className='flex items-center justify-center gap-1 text-white rounded px-5 py-1 font-medium text-base bg-purple-600 hover:bg-purple-500 transition-all duration-200 ease-in-out active:scale-95'
+                        >
+                            Sort {sortOrder === 'desc' ? 'Newest First' : 'Oldest First'} 
+                            <ArrowDownUp className='w-5 h-5' />
+                        </button>
+                    </div>
+
                     <div className="grid justify-items-center grid-cols-3 sm:grid-cols-4 lg:grid-cols-8 gap-4">
                         {filteredAvatars.map(avatar => (
                             <div
@@ -108,8 +136,8 @@ export const ProfilePage = () => {
                                     }`}
                             >
                                 <img
-                                    src={avatar.imageUrl}
-                                    alt={avatar.name}
+                                    src={avatar.imageUrl!}
+                                    alt={avatar.name!}
                                     className='w-20 rounded-md md:w-20 lg:w-24 lg:h-auto'
                                     loading='lazy'
                                 />
