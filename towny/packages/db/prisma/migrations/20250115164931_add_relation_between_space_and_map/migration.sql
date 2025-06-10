820b526eb3933198227ecbfe1356eac114edc22b
Om Sharma <mail.coderom@gmail.com>
2025-06-10 13:32:43 +0530

    REMOVE monorepo based app

--- towny/packages/db/prisma/migrations/20250115164931_add_relation_between_space_and_map/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Space" ADD COLUMN     "mapId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Space" ADD CONSTRAINT "Space_mapId_fkey" FOREIGN KEY ("mapId") REFERENCES "Map"("id") ON DELETE SET NULL ON UPDATE CASCADE;
